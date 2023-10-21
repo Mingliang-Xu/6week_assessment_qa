0b947343ad1e0eb668db5376329e5963b2c3d7c9
Mingliang-Xu <oliver.xml@hotmail.com>
2023-10-20 17:28:42 -0700

    6week assessment-qa

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -15,4 +15,37 @@ describe("Duel Duo tests", () => {
     await driver.get("http://localhost:8000");
     await driver.wait(until.titleIs("Duel Duo"), 1000);
   });
+  it('can display 5 robots to choose from', async() => {
+    await driver.get("http://localhost:8000");
+    await driver.findElement(By.id('draw')).click();
+    let botChoices = await driver.wait(until.elementLocated(By.id('choices')),1000)
+    let totalChoices = await botChoices.getAttribute('childElementCount')
+    expect(totalChoices).toEqual('5');
+    await driver.sleep(1000);
+  });
+  it('displays player duo after adding robots', async() => {
+    await driver.get("http://localhost:8000");
+    await driver.findElement(By.id('draw')).click();
+    await driver.sleep(1000);
+    await driver.findElement(By.className('bot-btn')).click();
+    await driver.sleep(1000);
+    await driver.findElement(By.className('bot-btn')).click();
+    let players= await driver.wait(until.elementLocated(By.id('player-duo')),1000)
+    expect(await players.getAttribute('childElementCount')).toEqual('2');
+  });
+
+  test('players goes back to choices after being removed from duo', async () => {
+    await driver.get("http://localhost:8000");
+    await driver.findElement(By.id('draw')).click();
+    await driver.sleep(1000);
+    await driver.findElement(By.className('bot-btn')).click();
+    await driver.sleep(1000);
+    await driver.findElement(By.className('bot-btn')).click();
+    await driver.sleep(1000);
+    await driver.findElement(By.xpath('//button[contains(text(),"Remove")]')).click();
+    let botChoices = await driver.wait(until.elementLocated(By.id('choices')),1000)
+    let totalChoices = await botChoices.getAttribute('childElementCount')
+    expect(totalChoices).toEqual('4');
+  });
+
 });
